c0b8912b36c4177b1231e2ca201dabf9c636e13d
pgirikishore <48059119+pgirikishore@users.noreply.github.com>
2024-02-07 05:28:32 +0000

    Added Merge Sort

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,22 @@ import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
-    int[] arr = {5, 2, 4, 6, 1, 3};
+    int[] arr1 = {5, 2, 4, 6, 1, 3};
+
+    System.out.println("Array before sorting: " + Arrays.toString(arr1));
+
+    // Sort the array using insertion sort
     InsertionSort sort = new InsertionSort();
-    sort.insertionSort(arr);
-    System.out.println(Arrays.toString(arr));
+    sort.insertionSort(arr1);
+    System.out.println("Array after sorting: " + Arrays.toString(arr1));
+
+
+    int[] arr2 = {5, 2, 4, 6, 1, 3};
+
+    System.out.println("Array before sorting: " + Arrays.toString(arr2));
+    // Sort the array using merge sort
+    MergeSort merge = new MergeSort();
+    merge.mergeSort(arr2, 0, arr2.length - 1);
+    System.out.println("Array after sorting: " + Arrays.toString(arr2));
   }
 }
